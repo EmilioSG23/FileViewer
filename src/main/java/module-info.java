32382186f5cd400a6dcdb4f2ff3a16db9b6a2180
EmilioSG23 <emiliosaenz2001@gmail.com>
2025-06-28 23:39:24 -0500

    Refactor FXML and CSS identifiers for consistency and clarity; update controller references and style definitions.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module com.emiliosg23 {
 
     opens com.emiliosg23 to javafx.fxml;
     exports com.emiliosg23;
+    opens com.emiliosg23.controllers to javafx.fxml;
+		exports com.emiliosg23.controllers;
 }
